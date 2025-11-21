83733a76c86a09ceb93006b9bd5e16db4805211f
Abdelwaheb <bouchahwaabdelwaheb@fsg.u-gabes.tn>
2025-11-21 15:57:20 +0100

    main

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:hackathonpeeug/screens/about_screen.dart';
+import 'package:hackathonpeeug/screens/chat_bot_page.dart';
+import 'package:hackathonpeeug/screens/home_page.dart';
+import 'package:hackathonpeeug/screens/login_screen.dart';
+import 'package:hackathonpeeug/screens/plans_screen.dart';
+import 'package:hackathonpeeug/screens/profile_screen.dart';
+import 'package:hackathonpeeug/screens/splash_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tawassol',
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/home': (context) => HomeScreen(),
+        '/profile': (context) => ProfileScreen(),
+        '/about': (context) => const AboutScreen(),
+        '/chatbot': (context) => const ChatBotPage(),
+        '/plans': (context) => const PlansScreen(),
+      },
+    );
+  }
+}
